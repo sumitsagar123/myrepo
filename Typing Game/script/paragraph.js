ba5d33ca2a54e7ff952b0487285eaa59f9728de5
sumitsagar123 <sumitsagar0010@gmail.com>
2023-04-14 20:54:17 +0530

    as

---
+++ Typing Game/script/paragraph.js
@@ -0,0 +1,13 @@
+const paragraph = [
+  "put all the necessary books in a bag my father always drops me at school my father always drops me at school",
+  "my father always drops me at school we are going to the principals cabin",
+  "what is the strength of this classroom the car we are going to the principals",
+  "we are going to the principals cabin we are going to the principals cabin what is the strength of this classroom",
+  "the principal is going to announce something what is the strength of this classroom",
+  "our class teacher is very good at teaching what is the strength of this classroom we are going to the principals cabin",
+  "she is looking towards the car we are going to the principals cabin what is the strength of this classroom",
+  "we are going to the principals cabin my friends what is the strength of this",
+  "nobody will go out in the darkness what is the strength of this classroom",
+  "there is no way to go outside we are going to the principals cabin we are going to the principals cabin",
+  "i am going to play cricket with my friends what is the strength of this classroom we are going to the principals cabin lorem",
+];
